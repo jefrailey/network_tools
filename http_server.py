2311fea61e608a5fe6e613cd4641189736808b99
jefrailey <jcaetof@gmail.com>
2014-06-10 16:32:08 -0700

    Add .open_socket() to HttpServer() to pass socket related tests

--- http_server.py
+++ http_server.py
@@ -1,3 +1,6 @@
+import socket
+
+
 class HttpServer(object):
     """docstring for HttpServer"""
     def __init__(self, ip=u'127.0.0.1', port=50000, backlog=5):
@@ -5,3 +8,11 @@ class HttpServer(object):
         self._port = port
         self._backlog = backlog
         self._socket = None
+
+    def open_socket(self):
+        self._socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+            socket.IPPROTO_IP)
+        self._socket.bind((self._ip, self._port))
+        self._socket.listen(self._backlog)
--- test_http.py
+++ test_http.py
@@ -15,10 +15,10 @@ def test_200_ok_byte():
 def test_socket_is_socket():
     s = HttpServer()
     s.open_socket()
-    assert isinstance(s._socket, socket)
+    assert isinstance(s._socket, socket.socket)
 
 
 def test_open_socket():
     s = HttpServer(ip=u'127.0.0.1', port=50000, backlog=5)
     s.open_socket()
-    assert s.getsockname() == ('127.0.0.1', 50000)
+    assert s._socket.getsockname() == ('127.0.0.1', 50000)
