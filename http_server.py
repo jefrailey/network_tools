2cddde0df757d62a2612f7741b8f4e46d810510b
jefrailey <jcaetof@gmail.com>
2014-06-10 16:23:30 -0700

    Update HttpServer() init to have default values; add tests for socket creation to test_http.py

--- http_server.py
+++ http_server.py
@@ -1,7 +1,7 @@
 class HttpServer(object):
     """docstring for HttpServer"""
     def __init__(self, ip=u'127.0.0.1', port=50000, backlog=5):
-        self.ip = ip
-        self.port = port
-        self.backlog = backlog
-        self.socket = None
+        self._ip = ip
+        self._port = port
+        self._backlog = backlog
+        self._socket = None
--- test_http.py
+++ test_http.py
@@ -1,4 +1,5 @@
 from http_server import HttpServer
+import socket
 
 
 def test_200_ok():
@@ -9,3 +10,15 @@ def test_200_ok():
 def test_200_ok_byte():
     s = HttpServer()
     assert isinstance(s.ok(), bytes)
+
+
+def test_socket_is_socket():
+    s = HttpServer()
+    s.open_socket()
+    assert isinstance(s._socket, socket)
+
+
+def test_open_socket():
+    s = HttpServer(ip=u'127.0.0.1', port=50000, backlog=5)
+    s.open_socket()
+    assert s.getsockname() == ('127.0.0.1', 50000)
