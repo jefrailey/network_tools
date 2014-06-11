814d440dd67e5eeabaa9a8213041d9157ab6fe50
jefrailey <jcaetof@gmail.com>
2014-06-10 18:10:19 -0700

    Add HttpServer.start_listening() to handle request/response cycle. Subclass Exception to create unique exceptions to aid flow control. Add tests to verify parsing and responses.

--- http_server.py
+++ http_server.py
@@ -1,6 +1,26 @@
 import socket
 
 
+class InvalidHttpCodeError(Exception):
+    """docstring for InvalidHttpCodeError"""
+    pass
+
+
+class NotGETRequestError(Exception):
+    """docstring for InvalidHttpCodeError"""
+    pass
+
+
+class NotHTTP1_1Error(Exception):
+    """docstring for InvalidHttpCodeError"""
+    pass
+
+
+class BadRequestError(Exception):
+    """docstring for InvalidHttpCodeError"""
+    pass
+
+
 class HttpServer(object):
     """docstring for HttpServer"""
     def __init__(self, ip=u'127.0.0.1', port=50000, backlog=5):
@@ -8,7 +28,16 @@ class HttpServer(object):
         self._port = port
         self._backlog = backlog
         self._socket = None
-        self._HTTPResponse = {200: 'OK'}
+        self._statusCodes = {
+            200: 'OK',
+            301: 'Moved Permanently',
+            304: 'Not Modified',
+            400: 'Bad Request',
+            404: 'Not Found',
+            405: 'Method Not Allowed',
+            500: 'Internal Server Error',
+            505: 'HTTP version not supported'
+            }
 
     def open_socket(self):
         self._socket = socket.socket(
@@ -23,5 +52,59 @@ class HttpServer(object):
         self._socket.close()
         self._socket = None
 
-    def gen_response(self, code=200):
-        return "HTTP/1.1 {} {}".format(code, self._HTTPResponse[code])
+    def gen_response(self, code, msg=None):
+        try:
+            if msg is None:
+                msg = self._statusCodes[code]
+            response = "HTTP/1.1 {} {}".format(code, msg)
+            return response
+        except KeyError:
+            raise InvalidHttpCodeError(
+                u'{} is not a valid HTTP code'.format(code))
+
+    def start_listening(self):
+        while True:
+            request = []
+            connection, addr = self._socket.accept()
+
+            while True:
+                buffer_ = connection.recv(32)
+                if buffer_:
+                    request.append(buffer_)
+                else:
+                    break
+            try:
+                self.parse_request(" ".join(request))
+            except NotGETRequestError:
+                connection.sendall(self.gen_response(405))
+            except BadRequestError:
+                connection.sendall(self.gen_response(400))
+            except NotHTTP1_1Error:
+                connection.sendall(self.gen_response(505))
+            else:
+                connection.sendall(self.gen_response(200))
+            finally:
+                connection.shutdown(socket.SHUT_RDWR)
+                connection.close()
+
+    def parse_request(self, request):
+        list_ = request.split("\r\n")
+        status_line = list_[0].split(" ")
+        if len(status_line) != 3:
+            raise BadRequestError
+        if status_line[0] != "GET":
+            raise NotGETRequestError
+        if status_line[2] != "HTTP/1.1":
+            raise NotHTTP1_1Error
+        return status_line[1]
+
+
+if __name__ == "__main__":
+    try:
+        s = HttpServer()
+        s.open_socket()
+        s.start_listening()
+    except KeyboardInterrupt:
+        s.close_socket()
+    finally:
+        s.close_socket()
--- test_http.py
+++ test_http.py
@@ -1,15 +1,21 @@
-from http_server import HttpServer
+from http_server import (
+    HttpServer,
+    NotGETRequestError,
+    NotHTTP1_1Error,
+    BadRequestError
+    )
 import socket
+import pytest
 
 
 def test_200_ok():
     s = HttpServer()
-    assert s.gen_response() == "HTTP/1.1 200 OK"
+    assert s.gen_response(200) == "HTTP/1.1 200 OK"
 
 
 def test_200_ok_byte():
     s = HttpServer()
-    assert isinstance(s.gen_response(), bytes)
+    assert isinstance(s.gen_response(200), bytes)
 
 
 def test_socket_is_socket():
@@ -29,3 +35,31 @@ def test_close_socket():
     s.open_socket()
     s.close_socket()
     assert s._socket is None
+
+
+def test_parse_one():
+    s = HttpServer()
+    assert s.parse_request("GET /uri/ HTTP/1.1") == "/uri/"
+
+
+def test_parse_2():
+    s = HttpServer()
+    with pytest.raises(NotGETRequestError):
+        s.parse_request("POST /uri/ HTTP/1.1")
+
+
+def test_parse_3():
+    s = HttpServer()
+    with pytest.raises(NotHTTP1_1Error):
+        s.parse_request("GET /uri/ HTTP/1.0")
+
+
+def test_parse_4():
+    s = HttpServer()
+    with pytest.raises(BadRequestError):
+        s.parse_request("GET/uri/HTTP/1.0")
+
+
+def test_gen_response_1():
+    s = HttpServer()
+    assert s.gen_response(301) == 'HTTP/1.1 301 Moved Permanently'
